efea0b2c081d7ea06ac8e8cee81fdc6c058c4d94
Juan Carlos Leto <jcletoar@gmail.com>
2019-09-20 22:14:39 -0300

    Modifico parametro para solucionar un deprecated

--- src/database.ts
+++ src/database.ts
@@ -4,9 +4,11 @@ import { mongodb } from './keys';
 export async function connect() {
     try {
         await mongoose.connect(mongodb.URI, {
+            useCreateIndex: true,
             useNewUrlParser: true
+        }).then(() => {
+            console.log('Conectado a la base de datos');
         });
-        console.log('Conectado a la base de datos');
     } catch (error) {
         
     }
