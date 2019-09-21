588d312aa9b1205f4e50084f49afe177ca49e5c0
Juan C Leto <jcletoar@gmail.com>
2019-09-20 22:38:31 -0300

    remuevo variable no utilizada

--- src/middleware/check-auth.js
+++ src/middleware/check-auth.js
@@ -3,7 +3,6 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
     try {
         const token = req.header.authorization;
-        console.log(secret);
         jwt.verify(token, "PutoElQueLee");
         next();
     } catch (error) {
